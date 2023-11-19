d87e0006f1df4cbd3a5fd3337a3d514acb665d55
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2023-11-19 20:44:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code (2023)
+
+## Usage
+
+```
+cargo run --bin 01 < fixtures/01.txt
+```
+
---
+++ src/bin/00.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, World!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn _01() {
+        // pass
+    }
+}
+
