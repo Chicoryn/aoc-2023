5c2ae377f8d46f8d0f67100bb2c9346796c7ca3c
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2023-12-01 19:48:31 +0000

    Day 1: Better solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,43 +1,47 @@
 use std::io::{self, BufRead};
 
-fn replace_spelled_out_digits(s: &str) -> String {
-    let mut result = String::new();
+fn pluck_spelled_out_digits(s: &str) -> Vec<u32> {
+    let mut result = vec! [];
 
-    for i in 0..s.len() {
+    for (i, ch) in s.chars().enumerate() {
         if s[i..].starts_with("one") {
-            result.push('1');
+            result.push(1);
         } else if s[i..].starts_with("two") {
-            result.push('2');
+            result.push(2);
         } else if s[i..].starts_with("three") {
-            result.push('3');
+            result.push(3);
         } else if s[i..].starts_with("four") {
-            result.push('4');
+            result.push(4);
         } else if s[i..].starts_with("five") {
-            result.push('5');
+            result.push(5);
         } else if s[i..].starts_with("six") {
-            result.push('6');
+            result.push(6);
         } else if s[i..].starts_with("seven") {
-            result.push('7');
+            result.push(7);
         } else if s[i..].starts_with("eight") {
-            result.push('8');
+            result.push(8);
         } else if s[i..].starts_with("nine") {
-            result.push('9');
+            result.push(9);
         } else if s[i..].starts_with("zero") {
-            result.push('0');
+            result.push(0);
+        } else if let Some(digit) = ch.to_digit(10) {
+            result.push(digit)
         } else {
-            result.push(s.chars().nth(i).unwrap());
+            // pass
         }
     }
 
     result
 }
 
-fn parse_calibration_value(s: &str) -> i32 {
-    let digits = s.chars()
+fn pluck_digits(s: &str) -> Vec<u32> {
+    s.chars()
         .filter_map(|ch| ch.to_digit(10))
-        .map(|d| d as i32)
-        .collect::<Vec<_>>();
+        .map(|d| d as u32)
+        .collect::<Vec<_>>()
+}
 
+fn parse_calibration_value(digits: &[u32]) -> u32 {
     10 * digits[0] + digits[digits.len() - 1]
 }
 
@@ -45,8 +49,8 @@ fn main() {
     let stdin = io::stdin().lock();
     let lines = stdin.lines().filter_map(Result::ok).collect::<Vec<_>>();
 
-    println!("{}", lines.iter().map(|line| parse_calibration_value(&line)).sum::<i32>());
-    println!("{}", lines.iter().map(|line| parse_calibration_value(&replace_spelled_out_digits(&line))).sum::<i32>());
+    println!("{}", lines.iter().map(|line| parse_calibration_value(&pluck_digits(&line))).sum::<u32>());
+    println!("{}", lines.iter().map(|line| parse_calibration_value(&pluck_spelled_out_digits(&line))).sum::<u32>());
 }
 
 #[cfg(test)]
@@ -62,8 +66,8 @@ mod tests {
             "treb7uchet",
         ];
 
-        assert_eq!(lines.iter().map(|line| parse_calibration_value(&line)).collect::<Vec<_>>(), &[12, 38, 15, 77]);
-        assert_eq!(lines.iter().map(|line| parse_calibration_value(&line)).sum::<i32>(), 142);
+        assert_eq!(lines.iter().map(|line| parse_calibration_value(&pluck_digits(&line))).collect::<Vec<_>>(), &[12, 38, 15, 77]);
+        assert_eq!(lines.iter().map(|line| parse_calibration_value(&pluck_digits(&line))).sum::<u32>(), 142);
     }
 
     #[test]
@@ -78,7 +82,12 @@ mod tests {
             "7pqrstsixteen"
         ];
 
-        assert_eq!(lines.iter().map(|line| parse_calibration_value(&replace_spelled_out_digits(&line))).collect::<Vec<_>>(), &[29, 83, 13, 24, 42, 14, 76]);
-        assert_eq!(lines.iter().map(|line| parse_calibration_value(&replace_spelled_out_digits(&line))).sum::<i32>(), 281);
+        assert_eq!(lines.iter().map(|line| parse_calibration_value(&pluck_spelled_out_digits(&line))).collect::<Vec<_>>(), &[29, 83, 13, 24, 42, 14, 76]);
+        assert_eq!(lines.iter().map(|line| parse_calibration_value(&pluck_spelled_out_digits(&line))).sum::<u32>(), 281);
+    }
+
+    #[test]
+    fn triple_spelled_digits() {
+        assert_eq!(pluck_spelled_out_digits("threeightwo"), &[3, 8, 2]);
     }
 }
