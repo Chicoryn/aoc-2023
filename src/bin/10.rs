00adc4d2cf0fea743981764044e45867561f1127
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2023-12-10 19:35:47 +0000

    Day 10: Use `ndarray` instead of `micro_ndarray`
    The `micro_ndarray` crate has a bit of an awkward API so change to
    using the _standard_ `ndarray` crate instead for a better future
    experience.

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,45 +1,45 @@
-use std::{io::{self, BufRead}, collections::VecDeque};
+use std::io::{self, BufRead};
 
-use micro_ndarray::Array;
+use ndarray::Array2;
 
 #[derive(Clone)]
 struct PipeGrid {
-    array: Array<char, 2>,
-    is_padding: Array<bool, 2>,
+    array: Array2<char>,
+    is_padding: Array2<bool>,
 }
 
 impl PipeGrid {
     fn parse(lines: &[String]) -> Option<Self> {
-        let is_padding = Array::new_with([lines[0].len() + 2, lines.len() + 2], false);
-        let array = Array::from_flat(
+        let is_padding = Array2::from_elem((lines.len(), lines[0].len()), false);
+        let array = Array2::from_shape_vec(
+            (lines.len(), lines[0].len()),
             lines.iter().flat_map(|line| line.chars()).collect(),
-            [lines[0].len(), lines.len()],
-        )?;
+        ).ok()?;
 
         Some(Self { array, is_padding })
     }
 
     fn padded(&self) -> Self {
-        let mut array = Array::new_with(
-            [self.array.size()[0] * 2, self.array.size()[1] * 2],
+        let mut array = Array2::from_elem(
+            [self.array.shape()[0] * 2, self.array.shape()[1] * 2],
             ' '
         );
-        let mut is_padding = Array::new_with(array.size(), false);
+        let mut is_padding = Array2::from_elem(array.raw_dim(), false);
 
-        for row in 0..self.array.size()[1] {
-            for col in 0..self.array.size()[0] {
-                is_padding[[col * 2 + 0, row * 2 + 0]] = self.is_padding[[col, row]];
-                is_padding[[col * 2 + 0, row * 2 + 1]] = true;
-                is_padding[[col * 2 + 1, row * 2 + 0]] = true;
-                is_padding[[col * 2 + 1, row * 2 + 1]] = true;
+        for row in 0..self.array.shape()[0] {
+            for col in 0..self.array.shape()[1] {
+                is_padding[[row * 2 + 0, col * 2 + 0]] = self.is_padding[[row, col]];
+                is_padding[[row * 2 + 0, col * 2 + 1]] = true;
+                is_padding[[row * 2 + 1, col * 2 + 0]] = true;
+                is_padding[[row * 2 + 1, col * 2 + 1]] = true;
 
-                array[[col * 2 + 0, row * 2 + 0]] = self.array[[col, row]];
-                array[[col * 2 + 1, row * 2 + 1]] = '.';
+                array[[row * 2 + 0, col * 2 + 0]] = self.array[[row, col]];
+                array[[row * 2 + 1, col * 2 + 1]] = '.';
                 [
-                    array[[col * 2 + 0, row * 2 + 1]],
-                    array[[col * 2 + 1, row * 2 + 0]],
+                    array[[row * 2 + 1, col * 2 + 0]],
+                    array[[row * 2 + 0, col * 2 + 1]],
                 ] =
-                    match self.array[[col, row]] {
+                    match self.array[[row, col]] {
                         'S' => ['S', 'S'],
                         '.' => ['.', '.'],
                         '|' => ['|', '.'],
@@ -56,23 +56,17 @@ impl PipeGrid {
         Self { array, is_padding }
     }
 
-    fn starting_point(&self) -> [usize; 2] {
-        self.array.iter()
-            .find_map(|(point, &ch)| {
-                if ch == 'S' {
-                    Some(point)
-                } else {
-                    None
-                }
-            })
+    fn starting_point(&self) -> (usize, usize) {
+        self.array.indexed_iter()
+            .find_map(|(point, &ch)| if ch == 'S' { Some(point) } else { None })
             .unwrap()
     }
 
-    fn neighbours(&self, point: [usize; 2]) -> impl Iterator<Item=[usize; 2]> + '_ {
-        let north = Some([point[0], point[1].wrapping_sub(1)]).filter(|&at| self.array.get(at).filter(|ch| ['|', '7', 'F', 'S'].contains(ch)).is_some());
-        let south = Some([point[0], point[1].wrapping_add(1)]).filter(|&at| self.array.get(at).filter(|ch| ['|', 'L', 'J', 'S'].contains(ch)).is_some());
-        let west = Some([point[0].wrapping_sub(1), point[1]]).filter(|&at| self.array.get(at).filter(|ch| ['-', 'L', 'F', 'S'].contains(ch)).is_some());
-        let east = Some([point[0].wrapping_add(1), point[1]]).filter(|&at| self.array.get(at).filter(|ch| ['-', 'J', '7', 'S'].contains(ch)).is_some());
+    fn neighbours(&self, point: (usize, usize)) -> impl Iterator<Item=(usize, usize)> + '_ {
+        let north = Some((point.0.wrapping_sub(1), point.1)).filter(|&at| self.array.get(at).filter(|ch| ['|', '7', 'F', 'S'].contains(ch)).is_some());
+        let south = Some((point.0.wrapping_add(1), point.1)).filter(|&at| self.array.get(at).filter(|ch| ['|', 'L', 'J', 'S'].contains(ch)).is_some());
+        let west = Some((point.0, point.1.wrapping_sub(1))).filter(|&at| self.array.get(at).filter(|ch| ['-', 'L', 'F', 'S'].contains(ch)).is_some());
+        let east = Some((point.0, point.1.wrapping_add(1))).filter(|&at| self.array.get(at).filter(|ch| ['-', 'J', '7', 'S'].contains(ch)).is_some());
 
         match self.array[point] {
             '|' => vec! [north, south],
@@ -86,10 +80,10 @@ impl PipeGrid {
         }.into_iter().filter_map(|x| x)
     }
 
-    fn traverse_pipe(&self) -> Array<usize, 2> {
+    fn traverse_pipe(&self) -> Array2<usize> {
         let starting_point = self.starting_point();
-        let mut remaining: Vec<[usize; 2]> = vec! [starting_point];
-        let mut distance_to = Array::new_with(self.array.size(), usize::MAX);
+        let mut remaining = vec! [starting_point];
+        let mut distance_to = Array2::from_elem(self.array.raw_dim(), usize::MAX);
         distance_to[starting_point] = 0;
 
         while let Some(point) = remaining.pop() {
@@ -108,24 +102,24 @@ impl PipeGrid {
         distance_to
     }
 
-    fn is_enclosed(&self, starting_point: [usize; 2], is_pipe: &Array<bool, 2>) -> (bool, Array<bool, 2>) {
-        let mut remaining = VecDeque::from([starting_point]);
-        let mut visited = Array::new_with(self.array.size(), false);
+    fn is_enclosed(&self, starting_point: (usize, usize), is_pipe: &Array2<bool>) -> (bool, Array2<bool>) {
+        let mut remaining = vec! [starting_point];
+        let mut visited = Array2::from_elem(self.array.raw_dim(), false);
         visited[starting_point] = true;
 
-        while let Some(point) = remaining.pop_front() {
+        while let Some(point) = remaining.pop() {
             let neighbours = [
-                [point[0], point[1].wrapping_sub(1)],
-                [point[0], point[1].wrapping_add(1)],
-                [point[0].wrapping_sub(1), point[1]],
-                [point[0].wrapping_add(1), point[1]],
+                (point.0, point.1.wrapping_sub(1)),
+                (point.0, point.1.wrapping_add(1)),
+                (point.0.wrapping_sub(1), point.1),
+                (point.0.wrapping_add(1), point.1),
             ];
 
             for neighbour in neighbours.into_iter() {
                 if self.array.get(neighbour) == None {
                     return (false, visited);
                 } else if !visited[neighbour] && !is_pipe[neighbour] {
-                    remaining.push_back(neighbour);
+                    remaining.push(neighbour);
                 }
 
                 visited[neighbour] = true;
@@ -136,15 +130,15 @@ impl PipeGrid {
     }
 
     fn enclosed_area(&self) -> usize {
-        let is_pipe = Array::from_flat(
-            self.traverse_pipe().iter().map(|(_, &distance)| distance != usize::MAX).collect(),
-            self.array.size(),
+        let is_pipe = Array2::from_shape_vec(
+            self.array.raw_dim(),
+            self.traverse_pipe().iter().map(|&distance| distance != usize::MAX).collect(),
         ).unwrap();
 
-        let mut enclosed = Array::new_with(self.array.size(), None);
+        let mut enclosed = Array2::from_elem(self.array.raw_dim(), None);
         let mut count = 0;
 
-        for (point, _) in self.array.iter() {
+        for (point, _) in self.array.indexed_iter() {
             if is_pipe[point] || self.is_padding[point] {
                 // pass
             } else if let Some(is_enclosed) = enclosed[point] {
@@ -152,16 +146,11 @@ impl PipeGrid {
                     count += 1;
                 }
             } else  {
-                let (is_enclosed, enclosed_) = self.is_enclosed(point, &is_pipe);
+                let (is_enclosed, visited) = self.is_enclosed(point, &is_pipe);
 
-                if is_enclosed {
-                    for (point, &is_enclosed_) in enclosed_.iter() {
-                        debug_assert!(enclosed[point] == None);
-
-                        enclosed[point] = Some(is_enclosed_);
-                    }
-
-                    count += 1;
+                count += if is_enclosed { 1 } else { 0 };
+                for (visited_point, _) in visited.indexed_iter().filter(|(_, &is_visited)| is_visited) {
+                    enclosed[visited_point] = Some(is_enclosed);
                 }
             }
         }
@@ -173,7 +162,8 @@ impl PipeGrid {
         let distance_to = self.traverse_pipe();
 
         distance_to.iter()
-            .filter_map(|(_, &distance)| if distance != usize::MAX { Some(distance) } else { None })
+            .copied()
+            .filter(|&distance| distance != usize::MAX)
             .max().unwrap()
     }
 }
@@ -181,7 +171,7 @@ impl PipeGrid {
 fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines().filter_map(Result::ok).collect::<Vec<_>>();
-    let pipe_grid: PipeGrid = PipeGrid::parse(&lines).unwrap();
+    let pipe_grid = PipeGrid::parse(&lines).unwrap();
 
     println!("{}", pipe_grid.max_distance());
     println!("{}", pipe_grid.padded().enclosed_area());
@@ -239,28 +229,28 @@ mod tests {
 
     #[test]
     fn _01() {
-        let pipe_grid: PipeGrid = PipeGrid::parse(&LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
+        let pipe_grid = PipeGrid::parse(&LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
 
         assert_eq!(pipe_grid.max_distance(), 8);
     }
 
     #[test]
     fn _02() {
-        let pipe_grid: PipeGrid = PipeGrid::parse(&LARGE_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
+        let pipe_grid = PipeGrid::parse(&LARGE_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
 
         assert_eq!(pipe_grid.padded().enclosed_area(), 8);
     }
 
     #[test]
     fn _02_squeeze() {
-        let pipe_grid: PipeGrid = PipeGrid::parse(&SQUEEZE_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
+        let pipe_grid = PipeGrid::parse(&SQUEEZE_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
 
         assert_eq!(pipe_grid.padded().enclosed_area(), 4);
     }
 
     #[test]
     fn _02_extra_tiles() {
-        let pipe_grid: PipeGrid = PipeGrid::parse(&EXTRA_TILES_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
+        let pipe_grid = PipeGrid::parse(&EXTRA_TILES_LINES.iter().map(|s| s.to_string()).collect::<Vec<_>>()).unwrap();
 
         assert_eq!(pipe_grid.padded().enclosed_area(), 10);
     }
